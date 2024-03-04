a57b8832a4ba2bcafc3a5c300a8319eb514b4528
Ekaterina Dudina <94860118+d-june@users.noreply.github.com>
2024-03-04 17:20:14 +0300

    created-project

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+// Подключение основного файла стилей
+import "../scss/style.scss";
+
+/* Динамический адаптив */
+import "./libs/dynamic_adapt.js";
+
+import "./slider.js";
+
+
+
+
+
---
+++ src/js/slider.js
@@ -0,0 +1,189 @@
+
+
+const sliderInit = (perPage, slider, items, sliderLine, nextButton, prevButton, currentSlide, allSlides, maxWidth, autoPlay, bullets ) => {
+
+    let width;
+
+
+    let current = perPage;
+    width = slider.offsetWidth;
+    let timerId = 0;
+    window.addEventListener('resize', () => {
+        init()
+    });
+ 
+
+    function init() {
+        sliderLine.style.width = width/perPage * items.length + 'px';
+
+    
+        items.forEach(item => {
+            if(item.length > 1) {
+                item.forEach(i => {
+                    i.style.width = width + 'px';
+                    i.style.height = 'auto';
+                })
+            } else {
+                item.style.width = width + 'px';
+                item.style.height = 'auto';
+            }
+       
+        });
+        currentSlide ? currentSlide.innerText = perPage : '';
+        allSlides ? allSlides.innerText = items.length : '';
+
+        if(bullets) {
+            bullets.innerHTML = ''
+            items.map((item) => {
+                bullets.innerHTML = bullets.innerHTML + '<div class="bullet"></div>'
+            })
+            bullets.childNodes[current-1].classList.add('bullet-active')
+        }
+        if(autoPlay) {
+            clearInterval(timerId);
+            autoplay()
+            rollSlider();
+        } else {
+            rollSlider();
+        }
+       
+    }
+
+
+
+    if(maxWidth) {
+        if(width <= maxWidth) {
+            init()
+        }
+    } else {
+        init();
+    }
+
+    nextButton.addEventListener('click', function () {
+        nextSlide();
+    });
+
+    function nextSlide () {
+        current = current + perPage;
+        currentSlide ? currentSlide.innerText = current : '';
+      
+        if (current > items.length ) {
+            current = perPage;
+            currentSlide ? currentSlide.innerText = current : '';
+        }
+
+        if(bullets) {
+            bullets.childNodes.forEach((bullet) => {
+                bullet.classList.remove('bullet-active')
+            })
+            bullets.childNodes[current-1].classList.add('bullet-active')
+        }
+        rollSlider();
+    }
+     
+    prevButton.addEventListener('click', function () {
+        clearInterval(timerId)
+       
+
+        if ( current / perPage * items.length  < items.length +1    ) {
+            current = items.length;
+            currentSlide ? currentSlide.innerText = current : '';
+        } 
+        
+        else{
+            current = current - perPage;
+            currentSlide ? currentSlide.innerText = current : '';
+        }
+       
+        if(bullets) {
+            bullets.childNodes.forEach((bullet) => {
+                bullet.classList.remove('bullet-active')
+            })
+            bullets.childNodes[current-1].classList.add('bullet-active')
+        }
+        
+       
+        rollSlider();
+    });
+    
+    function rollSlider() {
+       
+        sliderLine.style.transform = 'translate(-' + ((current - perPage)/perPage * width) + 'px)'; 
+    }
+
+    function  autoplay ()  {
+        timerId = setInterval(() => {
+            nextSlide ()
+           }, 4000);
+    }
+
+}
+
+
+const stagesSliderInit = () => {
+    const slider = document.querySelector('.stages__slider');
+    const items = document.querySelectorAll('.stages__item');
+    const groupItems = [[items[0], items[1]], items[2], [items[3], items[4]], items[5], items[6]]
+    const sliderLine = document.querySelector('.stages__items');
+    const nextButton = document.querySelector('.stages__slider-next');
+    const prevButton = document.querySelector('.stages__slider-prev');
+    const bullets = document.querySelector('.stages__slider-bullets');
+    const maxWidth = 767.98;
+
+    sliderInit(1, slider, groupItems, sliderLine, nextButton, prevButton, null, null, maxWidth, false, bullets)
+    window.addEventListener('resize', () => {
+        if (slider.offsetWidth < 767.98) {
+            sliderInit(1, slider, groupItems, sliderLine, nextButton, prevButton, null, null, maxWidth, false, bullets)
+        }
+    });
+}
+
+const stagesSlider = document.querySelector('.stages__slider');
+const stagesSliderWidth = stagesSlider.offsetWidth;
+
+if (stagesSliderWidth < 767.98) {
+    stagesSliderInit()
+}
+
+
+
+
+const participantsSliderInit = (perPage) => {
+    const slider = document.querySelector('.participants__slider');
+    const items = document.querySelectorAll('.participants__item');
+    const sliderLine = document.querySelector('.participants__items');
+    const currentSlide = document.querySelector('.participants__slider-current');
+    const allSlides = document.querySelector('.participants__slider-all');
+    const nextButton = document.querySelector('.participants__slider-next');
+    const prevButton = document.querySelector('.participants__slider-prev');
+
+        const width = slider.offsetWidth;
+
+        sliderInit(perPage, slider, items, sliderLine, nextButton, prevButton, currentSlide, allSlides, null, true, null)
+
+}
+
+const slider = document.querySelector('.participants__slider');
+    
+    const width = slider.offsetWidth;
+
+    if(width < 800 && width >= 600) {
+        participantsSliderInit(2)
+    } else if (width < 600) {
+        participantsSliderInit(1)
+    } else {
+        participantsSliderInit(3)
+    }
+
+window.addEventListener('resize', () => {
+    const slider = document.querySelector('.participants__slider');
+    
+    const width = slider.offsetWidth;
+    if(width < 800 && width >= 600) {
+        participantsSliderInit(2)
+    } else if (width < 600) {
+        participantsSliderInit(1)
+    } else {
+        participantsSliderInit(3)
+    }
+});
